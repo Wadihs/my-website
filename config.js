e524c8778fe4fe7fce328701d383e2f54e518dc2
Wadih Sader <31902402+Wadihs@users.noreply.github.com>
2026-08-19 09:21:31 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // IMPORTANT: If your current website already works, keep your existing config.js instead.
 // Only fill this in for a new installation.
-const SUPABASE_URL = "https://YOUR-PROJECT.supabase.co";
-const SUPABASE_PUBLIC_KEY = "YOUR-PUBLISHABLE-OR-ANON-KEY";
+const SUPABASE_URL = "https://nlclcjsdwlmopnutkcgz.supabase.co";
+const SUPABASE_PUBLIC_KEY = "sb_publishable_3ZeEjkoyHLQWh_TlDPCl5g_2yg5Bp7n";
